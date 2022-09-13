cbd973441b547f4efbafe71bb2648c992f57774d
seunghyun711 <seunghyun711@gmail.com>
2022-09-13 18:17:03 +0900

    Runtime measurement

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -25,13 +25,21 @@ public class MemberService {
      회원가입
      */
     public Long join(Member member){
-        // 같은 이름이 있는 중복 회원 X
-        validateDuplicateMember(member); // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
 
+        // 시간 측정
+        long start = System.currentTimeMillis();
+
+        try{
+            // 같은 이름이 있는 중복 회원 X
+            validateDuplicateMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
-
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                         .ifPresent(m -> {
@@ -43,7 +51,15 @@ public class MemberService {
      전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try{
+            return memberRepository.findAll();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
+
 
     }
 
